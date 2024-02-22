879a3f2261b2f12b2ba6af1ec0a63310283c4479
coderwin <gommind@naver.com>
2024-02-22 17:46:22 +0900

    feat first init about backend

---
+++ aws-test-back/src/test/java/com/example/awstestback/AwsTestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awstestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
