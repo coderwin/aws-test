094b6527de9fdadd4e64eec30efc3c39b84ba8e5
coderwin <gommind@naver.com>
2024-02-22 19:26:13 +0900

    feat 결과 반환 객체 생성

---
+++ aws-test-back/src/main/java/com/example/awstestback/util/Result.java
@@ -0,0 +1,32 @@
+package com.example.awstestback.util;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class Result<T> {
+
+    private T data;     // 결과값
+    private int code;   // 상태
+    private String message; // 결과 메시지
+
+    @Builder
+    public Result(T data, int code, String message) {
+        this.data = data;
+        this.code = code;
+        this.message = message;
+    }
+
+    // TODO : front에서 data가 unll인지 undefined 인지 확인 필요
+    public static Result<Void> success() {
+
+        Result<Void> result = Result.<Void>builder()
+                .code(HttpStatus.OK.value())
+                .build();
+
+        return result;
+    }
+
+
+}
